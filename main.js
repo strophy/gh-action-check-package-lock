b1e515f3e52c3543bb84d56dde2fddf89725c690
strophy <badfunkstripe@gmail.com>
2021-05-10 13:51:56 +0200

    try absolute path

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const packageJson = require('./package');
-const packageLockJson = require('./package-lock');
+const github = require('@actions/github');
+
+const packageJson = require('${{ github.workspace }}/package');
+const packageLockJson = require('${{ github.workspace }}./package-lock');
 
 console.log(packageJson);
