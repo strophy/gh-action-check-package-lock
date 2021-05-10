9449417f220036372c46103239a95d186b63f727
strophy <badfunkstripe@gmail.com>
2021-05-10 15:37:00 +0200

    compare using conditional statements

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const workspace = process.env.GITHUB_WORKSPACE;
 const packageJson = require(`${workspace}/package`);
 const packageLockJson = require(`${workspace}/package-lock`);
 
-try {
-  packageJson.name === packageLockJson.name;
-  packageJson.version === packageLockJson.version;
-} catch (error) {
-  core.setFailed(error.message);
+if (packageJson.name !== packageLockJson.name) {
+  core.setFailed('Package names do not match');
+}
+
+if (packageJson.version !== packageLockJson.version) {
+  core.setFailed('Package versions do not match');
 }
