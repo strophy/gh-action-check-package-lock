0ac24f3d29f3617a64df4e982133140cbc545bb5
strophy <badfunkstripe@gmail.com>
2021-05-10 14:56:06 +0200

    more debug output

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+const core = require('@actions/core');
 const github = require('@actions/github');
 
 // const packageJson = require(`${github.workspace}/package`);
 // const packageLockJson = require(`${github.workspace}/package-lock`);
 
 console.log(JSON.stringify(github));
-console.log('----------')
+console.log('----------');
 console.log(github.workspace);
+console.log('----------');
+console.log(JSON.stringify(core));
 // console.log(packageJson);
