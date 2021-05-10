87c9546a3d5be9aa4e3df5e7c7dd23bb74c54634
strophy <badfunkstripe@gmail.com>
2021-05-10 15:18:44 +0200

    finally write actual action code

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+
 const workspace = process.env.GITHUB_WORKSPACE;
+
 const packageJson = require(`${workspace}/package`);
 const packageLockJson = require(`${workspace}/package-lock`);
 
-console.log(JSON.stringify(github));
-console.log('----------');
-console.log(github.workspace);
-console.log('----------');
-console.log(JSON.stringify(core));
-console.log('----------');
-console.log(workspace);
-console.log('----------');
-console.log(process.env);
-console.log('----------');
-console.log(packageJson);
-console.log('----------');
-console.log(process.env)
+try {
+  packageJson.name === packageLockJson.name;
+  packageJson.version === packageLockJson.version;
+} catch (error) {
+  core.setFailed(error.message);
+}
